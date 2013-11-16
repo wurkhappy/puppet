495dfd025c77c2a28e2f982d6b443e39b22dcf02
root <root@li643-109.members.linode.com>
2013-11-16 21:03:40 +0000

    add postgres and daemontools modules

--- manifests/dbserver.pp
+++ manifests/dbserver.pp
@@ -4,3 +4,21 @@ class { 'timezone':
 	locality => 'UTC',
 }
 include ssh
+class { 'postgresql::globals':
+  manage_package_repo => true,
+  version             => '9.3',
+}->
+class { 'postgresql::server':
+  ip_mask_deny_postgres_user => '0.0.0.0/32',
+  ip_mask_allow_all_users    => '0.0.0.0/0',
+  listen_addresses           => '*',
+  postgres_password          => 'wh!coll@b',
+}
+postgresql::server::role { 'wurkhappy':
+  password_hash => postgresql_password('wurkhappy', 'whcollab'),
+}
+postgresql::server::db { 'wurkhappy':
+  user     => 'wurkhappy',
+  password => postgresql_password('wurkhappy', 'whcoll@b'),
+}
+include daemontools
