7475f4cf19b66845f5399447ca408f5ef2680b3e
root <root@li643-109.members.linode.com>
2013-11-17 17:29:51 +0000

    add init script

---
+++ init.sh
@@ -0,0 +1,15 @@
+~!/bin/bash
+sudo rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
+yum install puppet
+yum install git
+mkdir .ssh
+mv git-rsa .ssh/
+touch .ssh/config
+echo 'IdentityFile ~/.ssh/git_rsa' > .ssh/config
+eval `ssh-agent -s`
+ssh-add .ssh/git_rsa 
+chmod 400 .ssh/git_rsa 
+cd /etc/
+rm -rf puppet/
+git clone git@github.com:wurkhappy/puppet.git
+puppet apply /etc/puppet/manifests/dbserver.pp --modulepath=/etc/puppet/modules
