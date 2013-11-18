3edec4e50393c7bc472ed457e76e68aa47b3078a
MattParker89 <mdparker89@gmail.com>
2013-11-17 22:13:11 -0500

    Create services.pp

---
+++ manifests/services.pp
@@ -0,0 +1,25 @@
+stage { 'first': 
+        before => Stage['main'],
+}
+class{'basic_pkgs':
+        stage=> first,
+}
+include users
+class { 'timezone':
+        region => 'Etc',
+               locality => 'UTC',
+}
+include ssh
+include wh_firewall
+cron{'sync_puppet':
+        ensure => present,
+               command => 'cd /etc/puppet/ && /usr/bin/git pull origin master && /usr/bin/puppet apply /etc/puppet/manifests/dbserver.pp --modulepath=/etc/puppet/modules',
+               hour => '*',
+               minute => [0,30],
+}
+
+include '::rabbitmq'
+rabbitmq_plugin {'rabbitmq_admin':
+  ensure => present,
+}
+include zeromq
