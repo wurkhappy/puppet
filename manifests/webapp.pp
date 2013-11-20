365c60d1ed48adf8573dd7266f6f91af94a73781
MattParker89 <mdparker89@gmail.com>
2013-11-20 14:34:00 -0500

    Create webapp.pp

---
+++ manifests/webapp.pp
@@ -0,0 +1,44 @@
+stage { 'first': 
+        before => Stage['main'],
+}
+class{'basic_pkgs':
+        stage=> first,
+}
+include users
+class { 'timezone':
+        region => 'Etc',
+               locality => 'UTC',
+}
+include ssh
+include wh_firewall
+cron{'sync_puppet':
+        ensure => present,
+               command => 'cd /etc/puppet/ && /usr/bin/git pull origin master && /usr/bin/puppet apply /etc/puppet/manifests/webapp.pp --modulepath=/etc/puppet/modules',
+               hour => '*',
+               minute => [0,30],
+}
+
+include zeromq
+class{'golang':}
+->
+exec{'main-config':
+command => '/usr/bin/git clone git@github.com:wurkhappy/WH-Config.git',
+        cwd => '/root/go',
+            unless => ['/usr/bin/test -d /root/go/WH-Config'],
+}
+
+file{'/service':
+        ensure => 'directory',
+}
+
+wh_service{'WH-WebApp':
+        service_name => 'WH-WebApp',
+                     production => true,
+                     require => File['/service'],
+}
+wh_service::helper{'mdp':
+        repo => 'mdp',
+}
+class { 'redis':
+  version => '2.6.16',
+}
