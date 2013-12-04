370c2e49af20651daf7b5f942d2147b6ec1e8c24
MattParker89 <mdparker89@gmail.com>
2013-12-04 16:57:59 -0500

    Update init.pp

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -13,6 +13,7 @@ class ssh{
 		source => 'puppet:///modules/ssh/tunnel_rsa',
 		       owner => 'root',
 		       group => 'root',
+		       mode => 0700,
 	}
 	file_line{'tunnel_ident':
 		ensure => present,
