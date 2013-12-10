a96c9a550dfa392c500cafe9e27497d60384bcf7
root <root@li650-137.members.linode.com>
2013-12-10 23:44:48 +0000

    add package dependency for wkhtmltopdf

--- modules/wkhtmltopdf/manifests/init.pp
+++ modules/wkhtmltopdf/manifests/init.pp
@@ -1,4 +1,13 @@
 class wkhtmltopdf{
+	package{'urw-fonts':
+		ensure => installed,
+	}
+	package{'libXext':
+		ensure => installed,
+	}
+	package{'openssl-devel':
+		ensure => installed,
+	}	
 	exec { '/usr/bin/wget https://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.11.0_rc1-static-amd64.tar.bz2':
 		unless => ["/usr/bin/test -f /usr/local/bin/wkhtmltopdf"],
 		       notify => Exec['/bin/tar -C /usr/local/bin -jxzf wkhtmltopdf-0.11.0_rc1-static-amd64.tar.bz2'],
