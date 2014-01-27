6d02c6a790c1f91654220bf488baf76e6f49ca39
MattParker89 <mdparker89@gmail.com>
2014-01-27 16:34:40 -0500

    change permissions for email config files

--- modules/email/manifests/config.pp
+++ modules/email/manifests/config.pp
@@ -2,23 +2,23 @@ class email::config {
 	file{"/etc/postfix/main.cf":
 	ensure =>'file',
 	content => template('email/postfix_main.erb'),
-	mode => 0700,
+	mode => 0755,
 }
 file{"/etc/postfix/virtual":
 ensure =>'file',
 content => "enoch@${hostname}.wurkhappy.com   enoch\\@${hostname}.wurkhappy.com",
-mode => 0700,
+mode => 0755,
 }
 file{"/etc/sasl2/smtpd.conf":
 ensure =>'file',
 content => 'pwcheck_method: auxprop
 auxprop_plugin: sasldb
 mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5',
-mode => 0700,
+mode => 0755,
 }
 file{"/etc/imapd.conf":
 ensure =>'file',
 content => template('email/imapd.erb'),
-mode => 0700,
+mode => 0755,
 }
 }
