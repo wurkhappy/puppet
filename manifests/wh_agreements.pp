92ed7a52a928d907238959ab48f8707b9e95419f
Matt Parker <mdparker89@gmail.com>
2014-01-28 23:19:51 -0500

    change stages to wh_agreement

--- manifests/wh_agreements.pp
+++ manifests/wh_agreements.pp
@@ -17,11 +17,11 @@ exec{'main-config':
 command => '/usr/bin/git clone git@github.com:wurkhappy/WH-Config.git',
 cwd => '/home/wh',
 unless => ['/usr/bin/test -d /home/wh/WH-Config'],
-stage => 'app',
 }
 
 wh_service{'WH-Agreements':
 service_name => 'WH-Agreements',
 production => true,
 stage => 'app',
+require => Exec['main-config'],
 }
