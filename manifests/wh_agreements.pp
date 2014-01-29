e16e67d76d3ac62379bdef08fc9ec31bd78fe205
Matt Parker <mdparker89@gmail.com>
2014-01-28 23:11:01 -0500

    add rabbitmq and zeromq to wh_agreements

--- manifests/wh_agreements.pp
+++ manifests/wh_agreements.pp
@@ -1,3 +1,25 @@
+stage { 'tools':
+before => Stage['main'],
+}
+
+##Stage Tools
+class {'epel':
+stage => 'tools',
+}
+->
+package{'erlang':
+	ensure => installed,
+	stage => 'tools',
+}
+->
+class{'rabbitmq':
+stage => 'tools',
+}
+class{'zeromq':
+stage => 'tools',
+}
+
+
 exec{'main-config':
 	command => '/usr/bin/git clone git@github.com:wurkhappy/WH-Config.git',
 		cwd => '/home/wh',
