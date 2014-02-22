d5198e5f68562b58fee852ffe16b5e69101f0f89
Matt Parker <mdparker89@gmail.com>
2014-02-22 10:37:45 -0500

    add templates for mdp_broker daemon

--- modules/mdp_broker/manifests/init.pp
+++ modules/mdp_broker/manifests/init.pp
@@ -5,7 +5,7 @@ class mdp_broker($production = false){
 	file{'/service/mdp_broker/run':
 		ensure => 'file',
 		       mode => 0750,
-		       content => template('mdp_broker/service_run.erb'),
+		       content => template('mdp_broker/broker_run.erb'),
 	}
 	file{'/service/mdp_broker/log':
 		ensure => 'directory',
@@ -13,7 +13,7 @@ class mdp_broker($production = false){
 	file{'/service/mdp_broker/log/run':
 		ensure => 'file',
 		       mode => 0750,
-		       content => template('mdp_broker/service_log_run.erb'),
+		       content => template('mdp_broker/broker_log_run.erb'),
 	}
 	exec{'/usr/bin/git clone git@github.com:wurkhappy/mdp.git':
 		unless => '/usr/bin/test -d /home/wh/mdp',
